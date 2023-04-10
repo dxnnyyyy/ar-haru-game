54db4b3cb40d7fc5870b46068e00b834d86f0200
Daniel Koch <danielkoch@dnlkch.de>
2023-04-10 10:44:25 +0200

    chore(Index): updated index

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
+import MindARThreeViewer from "./components/mindar-three-viewer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App tab="home" />);
+root.render(
+  <div className="container">
+    <MindARThreeViewer />
+  </div>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
