57efa16f2e8c49896092bc8eca6285d10765f101
Daniel Koch <danielkoch@dnlkch.de>
2023-05-24 10:24:44 +0200

    switched to aframe

--- src/ARViewer.jsx
+++ src/ARViewer.jsx
@@ -1,37 +1,123 @@
-import React, { useEffect, useRef, useState } from "react";
-import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
-import ThreeCanvas from "./components/three-canvas";
+import React, { useState } from "react";
 
-export default function ARViewer(props) {
-  const { socket } = props;
+export default function ARViewer({ socket }) {
+  const [currentPlayer, setCurrentPlayer] = useState("player1");
+  const [harus, setHarus] = useState([]);
 
-  const containerRef = useRef(null);
-  const [mindarThree, setMindarThree] = useState();
+  const player = socket.auth.player;
 
-  useEffect(() => {
-    const mindarThree = new MindARThree({
-      container: containerRef.current,
-      imageTargetSrc: "/assets/targets_front.mind",
+  function handleTileClick(e) {
+    const position = e.target.components.position.data;
+
+    if (currentPlayer === player) {
+      const moveSuccsesful = updateGame(position);
+
+      if (moveSuccsesful) {
+        if (player === "player1") {
+          e.target.setAttribute("color", "blue");
+        }
+        if (player === "player2") {
+          e.target.setAttribute("color", "red");
+        }
+        socket.emit("player-moved", {
+          haruPosition: { x: position.x, y: position.y },
+        });
+      }
+    }
+  }
+
+  function updateGame(position) {
+    const haruExists = harus.find((haru) => {
+      return haru.props.position === `${position.x} ${position.y} 0.1`;
     });
 
-    setMindarThree(mindarThree);
+    if (!haruExists) {
+      setHarus([
+        ...harus,
+        <a-entity
+          key={`haru-${position.x}-${position.y}`}
+          gltf-model="url(/models/haru.glb)"
+          position={`${position.x} ${position.y} 0.1`}
+          rotation="90 0 0"
+          scale="0.2 0.2 0.2"
+        ></a-entity>,
+      ]);
+      const tile = document.querySelector(
+        `#haru-${position.x}${position.y} > a-plane`
+      );
+      if (currentPlayer === "player2") {
+        tile.setAttribute("color", "blue");
+      }
+      if (currentPlayer === "player1") {
+        tile.setAttribute("color", "red");
+      }
 
-    const { renderer, scene, camera } = mindarThree;
+      return true;
+    } else {
+      console.log("There is already a Haru on this tile!");
+      return false;
+    }
+  }
 
-    mindarThree.start();
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-    });
+  socket.on("switch-player", (data) => {
+    setCurrentPlayer(data.currentPlayer);
+  });
 
-    return () => {
-      renderer.setAnimationLoop(null);
-      mindarThree.stop();
-    };
-  }, []);
+  socket.on("move-done", (data) => {
+    updateGame(data.haruPosition);
+  });
+
+  function renderTiles(n) {
+    const tiles = [];
+    const offset = n % 2 === 0 ? n / 2 - 0.5 : Math.floor(n / 2);
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        tiles.push(
+          <a-entity
+            key={`${i}${j}`}
+            id={`haru-${i - offset}${j - offset}`}
+            onClick={handleTileClick}
+          >
+            <a-plane
+              color="white"
+              opaciy="0.25"
+              position={`${i - offset} ${j - offset} 0`}
+              width="0.8"
+              height="0.8"
+              rotation="0 0 0"
+              class="clickable"
+              animation="property: scale; to: 1.2 1.2 1.2; dur: 2500; easing: easeInOutQuad; loop: true; dir: alternate"
+            ></a-plane>
+          </a-entity>
+        );
+      }
+    }
+
+    return tiles;
+  }
 
   return (
-    <div style={{ width: "100%", height: "100%" }} ref={containerRef}>
-      {mindarThree && <ThreeCanvas mindarThree={mindarThree} socket={socket} />}
-    </div>
+    <>
+      {currentPlayer}
+      <a-scene
+        mindar-image="imageTargetSrc: /assets/targets_front.mind;"
+        vr-mode-ui="enabled: false"
+        color-space="sRGB"
+        renderer="colorManagement: true, physicallyCorrectLights"
+        device-orientation-permission-ui="enabled: false"
+      >
+        <a-camera
+          position="0 0 0"
+          look-controls="enabled: false"
+          cursor="fuse: false; rayOrigin: mouse;"
+          raycaster="far: 10000; objects: .clickable"
+        ></a-camera>
+
+        <a-entity mindar-image-target="targetIndex: 0">
+          {renderTiles(3)}
+          {harus}
+        </a-entity>
+      </a-scene>
+    </>
   );
 }
---
+++ src/ARViewer_Three.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from "react";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+import ThreeCanvas from "./components/three-canvas";
+
+export default function ARViewer(props) {
+  const { socket } = props;
+
+  const containerRef = useRef(null);
+  const [mindarThree, setMindarThree] = useState();
+
+  useEffect(() => {
+    const mindarThree = new MindARThree({
+      container: containerRef.current,
+      imageTargetSrc: "/assets/targets_front.mind",
+    });
+
+    setMindarThree(mindarThree);
+
+    const { renderer, scene, camera } = mindarThree;
+
+    mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+
+    return () => {
+      renderer.setAnimationLoop(null);
+      mindarThree.stop();
+    };
+  }, []);
+
+  return (
+    <div style={{ width: "100%", height: "100%" }} ref={containerRef}>
+      {mindarThree && <ThreeCanvas mindarThree={mindarThree} socket={socket} />}
+    </div>
+  );
+}
