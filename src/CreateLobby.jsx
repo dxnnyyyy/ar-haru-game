c18c0064bb227be614f534f69e0945fd8be71aac
Daniel Koch <danielkoch@dnlkch.de>
2023-05-25 11:55:27 +0200

    added beautiful buttons

--- src/CreateLobby.jsx
+++ src/CreateLobby.jsx
@@ -22,9 +22,22 @@ const ButtonContainer = styled.div`
   transform: translateX(-50%) translateY(-50%);
 `;
 
+const Button = styled.button`
+  color: black;
+  background: white;
+  font-weight: bold;
+  height: fit-content;
+  padding: 0.8rem 1.4rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  font-size: 1.2rem;
+  text-decoration: none;
+`;
+
 export default function CreateLobby(props) {
   const { socket } = props;
-  const room = createRoomId(6);
+  const room = createRoomId(4);
 
   socket.auth = { player: "player1", room: room };
   socket.connect();
@@ -45,13 +58,13 @@ export default function CreateLobby(props) {
       <LobbyCode>Lobby Code: {room}</LobbyCode>
 
       <ButtonContainer>
-        <button
+        <Button
           onClick={() => {
             socket.emit("start-game", room);
           }}
         >
           start game
-        </button>
+        </Button>
       </ButtonContainer>
     </>
   );
--- src/JoinLobby.jsx
+++ src/JoinLobby.jsx
@@ -17,6 +17,19 @@ const Input = styled.input`
   margin: 1rem;
 `;
 
+const Button = styled.button`
+  color: black;
+  background: white;
+  font-weight: bold;
+  height: fit-content;
+  padding: 0.8rem 1.4rem;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
+  font-size: 1.2rem;
+  text-decoration: none;
+`;
+
 function connectToLobby(socket, roomId) {
   socket.auth = { player: "player2", room: roomId };
   socket.connect();
@@ -42,13 +55,13 @@ export default function JoinLobby(props) {
       <FlexContainer>
         <label>Enter Lobby Code:</label>
         <Input ref={inputRef} name="lobbyCode" />
-        <button
+        <Button
           onClick={() => {
             connectToLobby(socket, inputRef.current.value);
           }}
         >
           join game
-        </button>
+        </Button>
       </FlexContainer>
     </>
   );
